ea814543ab471ab5f65dbf1ed2acd21ba40dc477
xiaocheng20 <yuchengtan0@gmail.com>
2021-10-03 22:43:24 +0800

    Remove redundant build scripts

--- build1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export CROSS_COMPILE=~/Toolchain/google_gcc/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-export ARCH=arm64
-export SUBARCH=arm64
-ls arch/arm64/configs
-read -p "choice your config : " defconfig
-make O=output $defconfig -j16
-make O=output -j16
